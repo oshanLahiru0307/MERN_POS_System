bba7c806387f374f1feb5a68fb668e9cd46297f0
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-01-12 22:27:17 +0530

    files are updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require("express")
 const CustomerRoutes = require('./routes/CustomerRoutes')
+const mongodb = require('mongoose')
 
 const app = express()
 
@@ -16,7 +17,19 @@ app.use((req,res,next)=>{
 //routes
 app.use('/api/customer', CustomerRoutes)
 
-//port listening..
-app.listen(process.env.PORT, ()=>{
+//connect mongodb..
+mongodb.conncect(MONGO_URI)
+.then( ()=> {
+
+    //port listening..
+    app.listen(process.env.PORT, ()=>{
     console.log("App is listening on Port",process.env.PORT)
 })
+
+})
+.catch((error)=> {
+    console.log(error)
+})
+
+
+
